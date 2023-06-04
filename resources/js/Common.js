0566a8f04122927b843c49044edc4bb68bb822e9
Francois Jaupitre <francois.jaupitre@gmail.com>
2023-06-04 02:43:33 -0300

    modulo 3

---
+++ resources/js/Common.js
@@ -0,0 +1,10 @@
+﻿function renderPage(url, articulos = {}) {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', url);
+    xhr.send();
+    xhr.addEventListener('load', () => {
+        const template = Handlebars.compile(xhr.response);
+        document.getElementById("view").innerHTML = template({articulos});
+
+    });
+}
